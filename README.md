4c287d693060ac2e763c163e0956706da715f8e3
Peter Marshall <petmshall@gmail.com>
2023-01-14 17:53:59 -0500

    Fix some stuff to get it working

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This is a calculator to convert the parameters of a peaking filter into IIR coefficients, in the fixed-point format that the max98090 audiuo codec requires.
 
 
-I have no idea if I'm doing this right, or if it will even work at all. Not for use in production!
+~~I have no idea if I'm doing this right, or if it will even work at all.~~ It's working, but it's likely that I'm still doing something completely wrong. Not for use in production!
--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,18 @@ struct FormattedCoefs {
 }
 
 impl FormattedCoefs {
+    fn to_dec(&self) -> String {
+        fn single_to_dec(val: &FormattedCoef) -> String {
+            format!("{} {} {}", val.value.0, val.value.1, val.value.2)
+        }
+        format!("{} {} {} {} {}",
+                single_to_dec(&self.b0),
+                single_to_dec(&self.b1),
+                single_to_dec(&self.b2),
+                single_to_dec(&self.a1),
+                single_to_dec(&self.a2))
+    }
+
 
     fn to_hex(&self) -> String {
         fn single_to_hex(val: &FormattedCoef) -> String {
@@ -49,7 +61,7 @@ impl FormattedCoefs {
 }
 
 impl BiquadFilter {
-    fn compute(&self) -> BiquadCoefficients {
+    fn compute_floats(&self) -> BiquadCoefficients {
         let bandwidth = self.frequency / self.q;
         let A = 10.0_f64.powf(self.gain / 40.0);
         let omega = 2.0 * PI * self.frequency / self.rate;
@@ -83,17 +95,14 @@ impl BiquadFilter {
             }
         };
 
-        println!("{} {} {}", coef, im, tc);
-
-        println!("{}", (tc as u32) >> 4);
 
         FormattedCoef {
-            value: ((tc >> 4 & 15) as u8, (((tc >> 12) & 15) | ((tc & 15) << 4)) as u8, (((tc >> 20) & 15) | (((tc >> 8) & 15) << 4)) as u8)
+            value: ((tc >> 16) as u8, (tc >> 8) as u8, tc as u8)
         }
     }
 
-    fn format(&self) -> FormattedCoefs {
-        let result = self.compute();
+    fn format(&self, result: BiquadCoefficients) -> FormattedCoefs {
+        //let result = self.compute();
         FormattedCoefs {
             a1: self.format_single(result.a1),
             a2: self.format_single(result.a2),
@@ -102,24 +111,52 @@ impl BiquadFilter {
             b2: self.format_single(result.b2),
         }
     }
+
+    fn compute(&self) -> FormattedCoefs {
+        let coefs = self.compute_floats();
+
+        self.format(coefs)
+    }
 }
 
 struct ThreeBandEQ (BiquadFilter, BiquadFilter, BiquadFilter);
 
-impl ThreeBandEQ {
-    //fn as_hex(&self) -> String {
-
-    //}
-}
-
 fn main() {
-    let test = BiquadFilter {
+    let band_1 = BiquadFilter {
         rate: 48000.0,
-        frequency: 265.0,
-        gain: 11.5,
+        frequency: 99.0,
+        gain: 5.01,
         q: 1.09,
     };
-    let result = test.format();
 
-    println!("{}", result.to_hex());
+    let band_2 = BiquadFilter {
+        rate: 48000.0,
+        frequency: 265.0,
+        gain: 7.51,
+        q: 1.09,
+    };
+
+    let band_3 = BiquadFilter {
+        rate: 48000.0,
+        frequency: 711.0,
+        gain: -0.44,
+        q: 1.09,
+    };
+
+    print!("{}", band_1.compute().to_dec());
+    print!(" {}", band_2.compute().to_dec());
+    print!(" {}", band_3.compute().to_dec());
+    
+    let pt = BiquadCoefficients {
+        a1: -2.0,
+        a2: 1.0,
+        b0: 1.0,
+        b1: -2.0,
+        b2: 1.0,
+    };
+    let t =  band_1.format(pt).to_dec();
+    for _ in 0..2 {
+        print!(" {}", t);
+    };
+    println!("");
 }
